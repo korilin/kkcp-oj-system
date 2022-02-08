c1ecf9a1f756bc7a4af79c2b9f75771499f080f3
korilin <korilin.dev@gmail.com>
2022-02-09 00:15:02 +0800

    add muse-ant-vue style

--- frontend/admin/src/App.vue
+++ frontend/admin/src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <a-button type="primary">
+      Primary
+    </a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed">
+      Dashed
+    </a-button>
+    <a-button danger>
+      Danger
+    </a-button>
+    <a-button type="link">
+      Link
+    </a-button>
+  </div>
 </template>
 
 <style>
--- frontend/admin/src/main.js
+++ frontend/admin/src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+// 覆盖 antd 样式
+import "./muse-ant-vue/app.scss";
+
+const app = createApp(App);
+
+app.use(Antd);
+
+app.mount("#app");
