04d7e7fecde040a6c55cc091a26f2b8492edce29
korilin <korilin.dev@gmail.com>
2022-02-14 22:52:55 +0800

    添加 header logo 和返回首页

---
+++ frontend/src/utils/router-helper.js
@@ -0,0 +1,5 @@
+import router from '../plugins/router';
+
+export const goHome = function() {
+    router.push('/')
+}
--- frontend/src/views/Error.vue
+++ frontend/src/views/Error.vue
@@ -1,23 +1,23 @@
 <script setup>
-function getQueryVariables() {
+import { goHome } from "../utils/router-helper";
+
+function getErrorInfo() {
     var query = window.location.search.substring(1);
     var vars = query.split("&");
-    var params = {};
     for (var i = 0; i < vars.length; i++) {
         var pair = vars[i].split("=");
-        params[pair[0]] = pair[1];
+        if (pair[0] == "info") {
+            return pair[1];
+        }
     }
-    return params;
+    return "发生了未知的错误";
 }
 </script>
 
 <template>
-    <a-result status="500" title="500" sub-title="Sorry, the server is wrong.">
+    <a-result status="error" title="发送错误" :sub-title="getErrorInfo()">
         <template #extra>
-            <a-button type="primary" @click="window.location.href='/'">Back Home</a-button>
+            <a-button type="primary" @click="goHome">Back Home</a-button>
         </template>
-        <div class="desc">
-            <div>{{ getQueryVariables() }}</div>
-        </div>
     </a-result>
 </template>
