bb4f0342cfaaf724f94dc0bd08d45ec98ecae888
korilin <korilin.dev@gmail.com>
2022-02-17 17:12:07 +0800

    mv user

--- frontend/user/src/main.js
+++ frontend/user/src/main.js
@@ -3,8 +3,8 @@ import App from "./App.vue";
 
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
-// 覆盖 antd 样式
-import "../muse-ant-vue/app.scss";
+// 使用 muse-ant-vue覆盖 antd 样式
+import "../../muse-ant-vue/app.scss";
 
 import router from "./plugins/router";
 
