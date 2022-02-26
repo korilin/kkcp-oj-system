aa2bb76ca7a3faa8f1a42cd9b8443f644d0ebac8
korilin <korilin.dev@gmail.com>
2022-02-26 15:35:17 +0800

    backend 添加 admin module

---
+++ backend/admin/build.gradle.kts
@@ -0,0 +1,10 @@
+parent?.let {
+    group = it.group
+    group = it.version
+}
+
+val ktorVersion = "1.6.7"
+
+dependencies{
+    implementation(project(":common"))
+}
--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "KotlinKnowledgeCompetition"
 include("application")
 include("user")
 include("common")
+include("admin")
