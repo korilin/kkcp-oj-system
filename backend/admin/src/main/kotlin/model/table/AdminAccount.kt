11e9a5c7fe32fe74016e977f1029a6bda8665ee5
korilin <korilin.dev@gmail.com>
2022-03-03 00:03:47 +0800

    迁移 AdminAccount 实体

--- backend/admin/src/main/kotlin/model/table/AdminAccount.kt
+++ backend/admin/src/main/kotlin/model/table/AdminAccount.kt
@@ -1,27 +1,29 @@
 package com.korilin.model.table
 
-import org.ktorm.entity.Entity
-import org.ktorm.schema.*
+import org.jetbrains.exposed.dao.EntityClass
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.javatime.datetime
 import java.time.LocalDateTime
-
-/**
- * 管理员实体
- */
-interface AdminAccount : Entity<AdminAccount> {
-    companion object : Entity.Factory<AdminAccount>()
-
-    var email: String
-    var name: String
-    var level: Int
-    var lastLoginTime: LocalDateTime
-}
 
 /**
  * 管理员账户表
  */
-object AdminAccounts : Table<AdminAccount>("admin_account") {
-    val email = varchar("email").primaryKey().bindTo { it.email }
-    val name = varchar("name").bindTo { it.name }
-    val level = int("level").bindTo { it.level }
-    val lastLoginTime = datetime("last_login_time").bindTo { it.lastLoginTime }
+object AdminAccounts : IntIdTable("admin_account") {
+    val email = varchar("email", 255)
+    val name = varchar("name", 45)
+    val level = integer("level")
+    val lastLoginTime = datetime("last_login_time")
+}
+
+/**
+ * 管理员实体
+ */
+class AdminAccount(id: EntityID<Int>) : IntEntity(id) {
+    companion object: EntityClass<Int, AdminAccount>(AdminAccounts)
+    var email: String by AdminAccounts.email
+    var name: String by AdminAccounts.name
+    var level: Int by AdminAccounts.level
+    var lastLoginTime: LocalDateTime by AdminAccounts.lastLoginTime
 }
