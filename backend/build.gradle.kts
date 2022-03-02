af6533b662b6faab5a237781f585e2991e2b161b
korilin <korilin.dev@gmail.com>
2022-03-02 23:19:21 +0800

    引入 java 时间扩展

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -55,6 +55,7 @@ allprojects {
                 implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
                 implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
                 implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+                implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
                 implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
                 implementation("mysql:mysql-connector-java")
 
