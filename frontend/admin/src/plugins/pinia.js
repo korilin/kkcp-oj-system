f0c064fedeab2af41ecf584f9b11a1157b831161
korilin <korilin.dev@gmail.com>
2022-03-05 23:48:41 +0800

    feature 添加代理和axios请求工具

--- frontend/admin/src/plugins/pinia.js
+++ frontend/admin/src/plugins/pinia.js
@@ -5,8 +5,7 @@ import { defineStore } from "pinia";
 export const useAccountStore = defineStore("account", {
     state: () => {
         return {
-            loginToken: null,
-            account: null,
+            kkcpAdminToken: null,
         };
     },
 });
--- frontend/admin/vite.config.js
+++ frontend/admin/vite.config.js
@@ -8,6 +8,12 @@ export default defineConfig({
     base: "/admin/",
     publicDir: "../public",
     server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:2021",
+                changeOrigin: true,
+            },
+        },
         fs: {
             // Allow serving files from one level up to the project root
             allow: [".."],
