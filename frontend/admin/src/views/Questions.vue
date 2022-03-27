10832ff52b9d3080f7daa6101bacbe231b99e138
korilin <korilin.dev@gmail.com>
2022-03-28 00:53:27 +0800

    Update Questions.vue

--- frontend/admin/src/views/Questions.vue
+++ frontend/admin/src/views/Questions.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useCommonStore, useQuestionsStore } from "../plugins/pinia"
+import Apis from "../utils/apis";
 import HttpService from "../utils/axios-service";
 
 const router = useRouter();
@@ -9,24 +10,24 @@ const questionsStore = useQuestionsStore();
 const commonStore = useCommonStore();
 
 const columns = [
-    {
-        title: "标题",
-        dataIndex: "title",
-        key: "title"
-    },
-    {
-        title: "题目类型",
-        dataIndex: "type",
-        key: "type"
-    },
-    {
-        title: "难度",
-        key: "level",
-    },
-    {
-        title: "ACTION",
-        key: "action",
-    },
+  {
+    title: "标题",
+    dataIndex: "title",
+    key: "title"
+  },
+  {
+    title: "题目类型",
+    dataIndex: "type",
+    key: "type"
+  },
+  {
+    title: "难度",
+    key: "level",
+  },
+  {
+    title: "ACTION",
+    key: "action",
+  },
 ];
 
 const levelsColor = [undefined, "green", "orange", "red"];
@@ -34,69 +35,65 @@ const levelsColor = [undefined, "green", "orange", "red"];
 const loading = ref(false);
 
 function goQuestionItem(questionId) {
-    router.push({
-        name: "question-item",
-        params: {
-            questionId: questionId
-        }
-    })
+  router.push({
+    name: "question-item",
+    params: {
+      questionId: questionId
+    }
+  })
 }
 
 function newQuestion() {
-    router.push({ name: "question-new" })
+  router.push({ name: "question-new" })
 }
 
 function initQuestionsData() {
-    loading.value = true;
-    HttpService.get("/admin/question/query/all").then((body) => {
-        if (body.status) {
-            questionsStore.data = body.data;
-            loading.value = false;
-        }
-    })
+  loading.value = true;
+  Apis.QuestionModule.queryQuestions().then((body) => {
+    if (body.status) {
+      questionsStore.data = body.data;
+      loading.value = false;
+    }
+  })
 }
 
 if (!questionsStore.init) {
-    initQuestionsData();
-    questionsStore.init = true;
+  initQuestionsData();
+  questionsStore.init = true;
 }
 </script>
 <template>
-    <div class="bar">
-        <h3 class="text-dark">Question Pool</h3>
-        <a-button type="primary" @click="newQuestion">Add</a-button>
-    </div>
-    <a-table
-        :columns="columns"
-        :data-source="questionsStore.data"
-        rowKey="questionId"
-        :loading="loading"
-    >
-        <template #bodyCell="{ column, record }">
-            <template v-if="column.key == 'type'">
-                <a-tag color="blue">{{ commonStore.getTypeById([record.type]).text }}</a-tag>
-            </template>
-            <template v-else-if="column.key == 'level'">
-                <a-tag
-                    :color="levelsColor[record.level]"
-                >{{ commonStore.getLevelById([record.level]).text }}</a-tag>
-            </template>
-            <template v-else-if="column.key == 'action'">
-                <a-button
-                    type="link"
-                    size="small"
-                    @click="goQuestionItem(record.questionId)"
-                >More&Edit</a-button>
-                <a-divider type="vertical" />
-                <a-button type="link" size="small" style="color: #ff7875;">Del</a-button>
-            </template>
-        </template>
-    </a-table>
+  <div class="bar">
+    <h3 class="text-dark">Question Pool</h3>
+    <a-button type="primary" @click="newQuestion">Add</a-button>
+  </div>
+  <a-table
+    :columns="columns"
+    :data-source="questionsStore.data"
+    rowKey="questionId"
+    :loading="loading"
+  >
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.key == 'type'">
+        <a-tag color="blue">{{ commonStore.getTypeById([record.type]).text }}</a-tag>
+      </template>
+      <template v-else-if="column.key == 'level'">
+        <a-tag
+          :color="levelsColor[record.level]"
+        >{{ commonStore.getLevelById([record.level]).text }}</a-tag>
+      </template>
+      <template v-else-if="column.key == 'action'">
+        <a-button type="link" size="small" @click="goQuestionItem(record.questionId)">More&Edit</a-button>
+        <a-divider type="vertical" />
+        <a-button type="link" size="small" style="color: #ff7875;">Del</a-button>
+      </template>
+    </template>
+  </a-table>
 </template>
 
 <style scoped lang="scss">
 .bar {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
