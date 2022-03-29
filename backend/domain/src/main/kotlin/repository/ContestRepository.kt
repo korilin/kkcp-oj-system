fa64e44f810faec405aeae905747163e3ae433df
korilin <korilin.dev@gmail.com>
2022-03-29 22:02:54 +0800

    Update ContestRepository.kt

--- backend/domain/src/main/kotlin/repository/ContestRepository.kt
+++ backend/domain/src/main/kotlin/repository/ContestRepository.kt
@@ -1,10 +1,43 @@
 package com.korilin.repository
 
+import com.korilin.table.Contest
+import com.korilin.table.Contests
 import org.ktorm.database.Database
+import org.ktorm.dsl.eq
+import org.ktorm.dsl.map
+import org.ktorm.dsl.select
+import org.ktorm.entity.add
+import org.ktorm.entity.find
 import org.springframework.stereotype.Repository
 
 @Repository
 class ContestRepository(database: Database) {
     private val contests = database.contests
     private val contestsSource = database.contestsSource
+
+    fun findContestById(contestId: Int) = contests.find {
+        it.contestId eq contestId
+    }
+
+    fun queryContests() = contestsSource.select(Contests.columns).map {
+        Contests.createEntity(it)
+    }
+
+    fun newContest(contest: Contest) {
+        contests.add(contest)
+    }
+
+    fun updateContest(contestId: Int): Int {
+        val contest = findContestById(contestId) ?: return 0
+
+        return contest.flushChanges()
+    }
+
+    companion object {
+        var a = 1
+
+        fun hei() {
+            this.a = 1
+        }
+    }
 }
