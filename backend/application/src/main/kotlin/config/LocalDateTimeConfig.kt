c4109763919ef74a01cd1e90b578aa4ee8e19968
korilin <korilin.dev@gmail.com>
2022-04-04 09:36:25 +0800

    添加 LocalDateTime序列化

---
+++ backend/application/src/main/kotlin/config/LocalDateTimeConfig.kt
@@ -0,0 +1,25 @@
+package com.korilin.config
+
+import com.fasterxml.jackson.databind.Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import com.korilin.LOCAL_DATE_TIME_PATTERN
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Configuration
+class LocalDateTimeConfig {
+
+    @Bean
+    fun formatter() : Module{
+        return JavaTimeModule().apply {
+            val jClass = LocalDateTime::class.java
+            val formatter = DateTimeFormatter.ofPattern(LOCAL_DATE_TIME_PATTERN)
+            addDeserializer(jClass, LocalDateTimeDeserializer(formatter))
+            addSerializer(jClass, LocalDateTimeSerializer(formatter))
+        }
+    }
+}
--- backend/common/src/main/kotlin/module.kt
+++ backend/common/src/main/kotlin/module.kt
@@ -1,3 +1,5 @@
 package com.korilin
 
 const val ANNOTATION_PACKAGE = "com.korilin.annotations"
+
+const val LOCAL_DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss"
