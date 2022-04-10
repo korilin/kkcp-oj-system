410a68a155b5d415434b10ae4ceb46552f05f231
korilin <korilin.dev@gmail.com>
2022-04-10 17:05:28 +0800

    更正逻辑

--- backend/domain/src/main/kotlin/repository/ContestRepository.kt
+++ backend/domain/src/main/kotlin/repository/ContestRepository.kt
@@ -7,6 +7,7 @@ import com.korilin.table.Contests
 import org.ktorm.database.Database
 import org.ktorm.dsl.eq
 import org.ktorm.dsl.map
+import org.ktorm.dsl.or
 import org.ktorm.dsl.select
 import org.ktorm.entity.add
 import org.ktorm.entity.find
@@ -28,21 +29,24 @@ class ContestRepository(database: Database) {
 
     fun newContest(contest: Contest) = contests.add(contest) == 1
 
+    fun findReleaseContest() = contests.find {
+        it.status eq ContestStatus.RELEASE.id
+    }
+
     @Transactional
     suspend fun updateStatus(contest: Contest, status: ContestStatus): Int {
-        if (status == ContestStatus.RELEASE || status == ContestStatus.COMPLETE) {
+        if (status == ContestStatus.RELEASE || status == ContestStatus.UNDERWAY) {
             synchronized(this) {
-                val f = contests.find { it.status eq status.id }
+                val f = contests.find {
+                    (it.status eq ContestStatus.RELEASE.id) or (it.status eq ContestStatus.UNDERWAY.id)
+                }
                 if (f != null) {
                     throw AbnormalStatusException("There has a ${status.text} contest ${f.contestId}_${f.title}")
                 }
-                contest.status = status.id
-                return contest.flushChanges()
             }
-        } else {
-            contest.status = status.id
-            return contest.flushChanges()
         }
+        contest.status = status.id
+        return contest.flushChanges()
     }
 }
 
