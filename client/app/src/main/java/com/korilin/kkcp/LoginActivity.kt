ac5cf5ab4657a048195ff77584a08cfe870a9ca0
korilin <korilin.dev@gmail.com>
2022-04-24 02:48:12 +0800

    发送布局

--- client/app/src/main/java/com/korilin/kkcp/LoginActivity.kt
+++ client/app/src/main/java/com/korilin/kkcp/LoginActivity.kt
@@ -2,12 +2,17 @@ package com.korilin.kkcp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.textfield.TextInputEditText
 import com.korilin.kkcp.databinding.ActivityLoginBinding
+import com.korilin.kkcp.network.LoginBody
+import com.korilin.kkcp.network.httpService
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +22,19 @@ class LoginActivity : AppCompatActivity() {
             loginBtn.setOnClickListener {
                 val email = emailInput.editText?.text.toString()
                 val code = codeInput.editText?.text.toString()
-                
+
+                lifecycleScope.launch {
+                    try {
+                        val response = httpService.login(LoginBody(email, code))
+                        if (response.status) {
+                            println(response.data)
+                        } else {
+                            println(response.message)
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
             }
         }
         setContentView(binding.root)
